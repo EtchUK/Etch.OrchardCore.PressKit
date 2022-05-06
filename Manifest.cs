10895a3d8a7e81286fedb4dde65996cb21d98605
Peter Keating <pete.keating@etchuk.com>
2022-05-06 15:27:07 +0100

    Upgrade Orchard Core to 1.3

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Simplify creating press kits for games industry.",
     Name = "Press Kit",
-    Version = "1.1.0",
+    Version = "1.2.0",
     Website = "https://etchuk.com",
     Dependencies = new string[] { "Etch.OrchardCore.Blocks" }
 )]

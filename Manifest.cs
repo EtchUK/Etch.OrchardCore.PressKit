be57371398e5cbcf884807aa2315b293e9518b6b
Peter Keating <pete.keating@etchuk.com>
2023-01-23 08:47:12 +0000

    Use git tag name to populate version number during deployment

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Simplify creating press kits for games industry.",
     Name = "Press Kit",
-    Version = "1.3.0",
+    Version = "$(VersionNumber)",
     Website = "https://etchuk.com",
     Dependencies = new string[] { "Etch.OrchardCore.Blocks" }
 )]
